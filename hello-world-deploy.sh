ca34916477af165bbbdccc22894f663fa54b43c3
jstolc <85391603+jstolc@users.noreply.github.com>
2021-06-05 17:53:08 +0100

    Update hello-world-deploy.sh

--- hello-world-deploy.sh
+++ hello-world-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+systemctl enable docker
+systemctl start docker
 docker build -t helloworld .
 docker run -d -p 80:8080 helloworld 
 
