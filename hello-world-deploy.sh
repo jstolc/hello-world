aabeec08585c00653799e8a08076556849dafe9b
jstolc <85391603+jstolc@users.noreply.github.com>
2021-06-05 21:13:45 +0100

    Update hello-world-deploy.sh

--- hello-world-deploy.sh
+++ hello-world-deploy.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+
+# download and install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+# start the service
 systemctl enable docker
 systemctl start docker
+# build container
 docker build -t helloworld .
+# run the web server along with the servlet 
 docker run -d -p 80:8080 helloworld 
-IP=$(ping -c 1 `hostname` | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
-echo 
-echo To access hello-world, navigate to http://"${IP}"/hello
+sleep 20  # this cannot hurt that much :)
+
+# test the result
+S1=$(curl -fsSL http://localhost/hello)
+S2='Hello, World!'
+if [ "$S1" == "$S2" ]
+then
+  # display success message
+  IP=$(ping -c 1 `hostname` | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
+  printf "\nTo access hello-world web app, navigate to http://${IP}/hello\n"   
+else 
+  # bad luck
+  echo Something went wrong.
+fi
+
 
