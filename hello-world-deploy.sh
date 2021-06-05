b0607e8da0508264b7a17c7666f36768c3b0f37f
jstolc <85391603+jstolc@users.noreply.github.com>
2021-06-05 20:49:47 +0100

    Update hello-world-deploy.sh

--- hello-world-deploy.sh
+++ hello-world-deploy.sh
@@ -5,4 +5,7 @@ systemctl enable docker
 systemctl start docker
 docker build -t helloworld .
 docker run -d -p 80:8080 helloworld 
+IP=$(ping -c 1 `hostname` | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
+echo 
+echo To access hello-world, navigate to http://"${IP}"/hello
 
